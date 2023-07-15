8125bcdf2e388ebac40ccdad58bfb8e272131889
Raitab <alexschram2@gmail.com>
2023-07-14 20:38:45 -0400

    Major rewrite. Fix broken things. Add Friend Notes option. Update README.

--- src/main/java/com/FriendsExporter/FriendsExporterConfig.java
+++ src/main/java/com/FriendsExporter/FriendsExporterConfig.java
@@ -8,59 +8,42 @@ import net.runelite.client.config.ConfigItem;
 public interface FriendsExporterConfig extends Config
 {
 	@ConfigItem(
-			keyName = "lineStart",
-			name = "New entry",
-			description = "What to add before each player.",
-			position = 1
+		keyName = "lineStart",
+		name = "Line Start",
+		description = "What format to add at the beginning of a line.",
+		position = 1
 	)
-	default LineLeads Lineleads() { return LineLeads.None;}
+	default LineLeads lineLeads() {return LineLeads.None;}
+
 	@ConfigItem(
-			keyName = "prev",
-			name = "Show Previous Names",
-			description = "Shows the previous name if available.",
-			position = 2
+		keyName = "prev",
+		name = "Include Previous Names",
+		description = "Shows the previous name if available.",
+		position = 2
 	)
-	default boolean prevName()	{return true;}
+	default boolean includePrevName() {return true;}
+
 	@ConfigItem(
-		keyName = "separator",
-		name = "Name Separator",
-		description = "Separator between current and previous names.",
+		keyName = "note",
+		name = "Include Note",
+		description = "Includes note from Friend Notes plugin on Friends List export.",
 		position = 3
 	)
-	default String Separator()
-	{
-		return "/";
-	}
-	@ConfigItem(
-			keyName = "unrank",
-			name = "Show unranked players",
-			description = "Shows players that do not have a rank but are still friends in ranks export.",
-			position = 4
-	)
-	default boolean showUnranked()
-	{
-		return false;
-	}
-	@ConfigItem(
-			keyName = "line",
-			name = "Separate lines",
-			description = "Separates entrys with new lines.",
-			position = 5
-	)
-	default boolean newLine()	{return false;}
-	@ConfigItem(
-			keyName = "1",
-			name = "Separate lines",
-			description = "Separates entrys with new lines.",
-			position = 6
-	)
-	default int newWid()	{return 0;}
-	@ConfigItem(
-			keyName = "2",
-			name = "Separate lines",
-			description = "Separates entrys with new lines.",
-			position = 7
-	)
-	default int newGet()	{return 0;}
+	default boolean includeNote() {return true;}
 
+	@ConfigItem(
+		keyName = "unrank",
+		name = "Show Unranked Players",
+		description = "Shows players that do not have a rank but are still friends in ranks export.",
+		position = 4
+	)
+	default boolean showUnranked() {return false;}
+
+	@ConfigItem(
+		keyName = "separator",
+		name = "Separator",
+		description = "Separator between fields for a player.",
+		position = 5
+	)
+	default String getSeparator() {return " - ";}
 }
--- src/main/java/com/FriendsExporter/LineLeads.java
+++ src/main/java/com/FriendsExporter/LineLeads.java
@@ -1,14 +1,15 @@
 package com.FriendsExporter;
 
 public enum LineLeads {
-    None("None"),
-    Number("[Number]"),
-    Number1("[Number]."),
-    Number2("[Number])"),
-    Number3("[Number].)")
+    None("None", ""),
+    Number("[Number]", " "),
+    Number1("[Number].", ". "),
+    Number2("[Number])", ") "),
+    Number3("[Number].)", ".) ")
     ;
 
     private final String name;
+    private final String punctuation;
 
     public String toString() {
         return this.getName();
@@ -17,8 +18,10 @@ public enum LineLeads {
     public String getName() {
         return this.name;
     }
+    public String getPunctuation() {return this.punctuation;}
 
-    private LineLeads(String name) {
+    private LineLeads(String name, String punctuation) {
         this.name = name;
+        this.punctuation = punctuation;
     }
 }
